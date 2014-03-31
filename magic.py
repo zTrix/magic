b866abc2421e585d134e76a29510b4b30a69076d
zTrix <i@ztrix.me>
2014-03-31 14:22:15 +0800

    add mmap

--- magic.py
+++ magic.py
@@ -10,45 +10,62 @@ except:
     def colored(text, color=None, on_color=None, attrs=None):
         return text
 
+TYPE_EQUAL = 'equal'
+TYPE_BITOR = 'bitor'
+
 py_magic = {
     'os': {
         'open': {
             'flags': 'O_APPEND O_ASYNC O_CREAT O_DIRECTORY O_DSYNC O_EXCL O_EXLOCK O_NDELAY O_NOCTTY O_NOFOLLOW O_NONBLOCK O_RDONLY O_RDWR O_SHLOCK O_SYNC O_TRUNC O_WRONLY'.split(' '),
-            'type': 'bitor'
+            'type': TYPE_BITOR
         },
         'seek': {
             'flags': 'SEEK_SET SEEK_CUR SEEK_END'.split(' '),
-            'type': 'equal'
+            'type': TYPE_EQUAL
         }
     },
     'termios': {
         'iflags': {
             'flags': 'IGNBRK BRKINT IGNPAR PARMRK INPCK ISTRIP INLCR IGNCR ICRNL IUCLC IXON IXANY IXOFF IMAXBEL IUTF8'.split(' '),
-            'type': 'bitor'
+            'type': TYPE_BITOR
         },
         'oflags': {
             'flags': 'OPOST OLCUC ONLCR OCRNL ONOCR ONLRET OFILL OFDEL NLDLY CRDLY TABDLY BSDLY VTDLY FFDLY'.split(' '),
-            'type': 'bitor',
+            'type': TYPE_BITOR,
         },
         'cflags': {
             'flags': 'CBAUD CBAUDEX CSIZE CSTOPB CREAD PARENB PARODD HUPCL CLOCAL LOBLK CIBAUD CMSPAR CRTSCTS'.split(' '),
-            'type': 'bitor',
+            'type': TYPE_BITOR,
         },
         'lflags': {
             'flags': 'ISIG ICANON XCASE ECHO ECHOE ECHOK ECHONL ECHOCTL ECHOPRT ECHOKE DEFECHO FLUSHO NOFLSH TOSTOP PENDIN IEXTEN'.split(' '),
-            'type': 'bitor',
+            'type': TYPE_BITOR,
         }
     },
     'signal': {
         'flags': 'NSIG SIGABRT SIGALRM SIGBUS SIGCHLD SIGCONT SIGEMT SIGFPE SIGHUP SIGILL SIGINFO SIGINT SIGIO SIGIOT SIGKILL SIGPIPE SIGPROF SIGQUIT SIGSEGV SIGSTOP SIGSYS SIGTERM SIGTRAP SIGTSTP SIGTTIN SIGTTOU SIGURG SIGUSR1 SIGUSR2 SIGVTALRM SIGWINCH SIGXCPU SIGXFSZ SIG_DFL SIG_IGN'.split(' '),
-        'type': 'equal'
+        'type': TYPE_EQUAL
+    },
+    'mmap': {
+        'access': {
+            'flags': 'ACCESS_COPY ACCESS_READ ACCESS_WRITE'.split(' '),
+            'type': TYPE_BITOR,
+        },
+        'prot': {
+            'flags': 'PROT_EXEC PROT_READ PROT_WRITE'.split(' '),
+            'type': TYPE_BITOR,
+        },
+        'flags': {
+            'flags': 'MAP_ANON MAP_ANONYMOUS MAP_DENYWRITE MAP_EXECUTABLE MAP_PRIVATE MAP_SHARED'.split(' '),
+            'type': TYPE_BITOR,
+        }
     }
 }
 
 magics = {
     'ascii': {
         'flags': [(0, 'nul'), (1, 'soh'), (2, 'stx'), (3, 'etx'), (4, 'eot'), (5, 'enq'), (6, 'ack'), (7, 'bel'), (8, 'bs'), (9, 'ht'), (10, 'nl'), (11, 'vt'), (12, 'np'), (13, 'carriage return'), (14, 'so'), (15, 'si'), (16, 'dle'), (17, 'dc1'), (18, 'dc2'), (19, 'dc3'), (20, 'dc4'), (21, 'nak'), (22, 'syn'), (23, 'etb'), (24, 'can'), (25, 'em'), (26, 'sub'), (27, 'esc'), (28, 'fs'), (29, 'gs'), (30, 'rs'), (31, 'us'), (32, 'space'), (127, 'delete')] + [(x, chr(x)) for x in range(33, 127)],
-        'type': 'equal'
+        'type': TYPE_EQUAL
     }
 }
 
@@ -89,7 +106,7 @@ def magic(number, hints, match = FIND):
         def visit(obj, path):
             if 'flags' in obj and 'type' in obj:
                 if not match_all(path): return
-                if obj['type'] == 'equal':
+                if obj['type'] == TYPE_EQUAL:
                     bits = set()
                     for f in obj['flags']:
                         try:
@@ -98,7 +115,7 @@ def magic(number, hints, match = FIND):
                         except: pass
                     if bits:
                         ret[path] = bits
-                elif obj['type'] == 'bitor':
+                elif obj['type'] == TYPE_BITOR:
                     bits = []
                     for f in obj['flags']:
                         try:
@@ -119,14 +136,14 @@ def magic(number, hints, match = FIND):
     for key in magics:
         if not match_all(key):
             continue
-        if magics[key]['type'] == 'equal':
+        if magics[key]['type'] == TYPE_EQUAL:
             bits = set()
             for n, s in magics[key]['flags']:
                 if n == number:
                     bits.add(s)
             if bits:
                 ret[key] = bits
-        elif magics[key]['type'] == 'bitor':
+        elif magics[key]['type'] == TYPE_BITOR:
             bits = []
             for n, s in magics[key]['flags']:
                 if n & number:
